cd49b9b081906e0670024c991ba8f08b2ba377cd
iqnefo1310 <iqbalnafis13@gmail.com>
2025-03-15 05:12:33 +0700

    save tugas to development

--- obj/Debug/net8.0/UITraining.AssemblyInfo.cs
+++ obj/Debug/net8.0/UITraining.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d28648fc4162b2c667489639f8a6e6467d85699")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22925b0df25872d2fc47e4c94c3a14d5ce518837")]
 [assembly: System.Reflection.AssemblyProductAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UITraining")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
